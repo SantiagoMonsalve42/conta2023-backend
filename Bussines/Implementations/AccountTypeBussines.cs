9f0796f10e723c2a00e08bc78f4e0ec25b206811
Andres Santiago Monsalve <santi.monsalve09@gmail.com>
2023-11-26 12:23:19 -0500

    enpoint completo

--- Bussines/Implementations/AccountTypeBussines.cs
+++ Bussines/Implementations/AccountTypeBussines.cs
@@ -1,6 +1,8 @@
-﻿using Bussines.Interfaces;
+﻿using Azure.Core;
+using Bussines.Interfaces;
 using Data.Implementations;
 using Data.Models;
+using DTO.request;
 using DTO.response;
 using Util;
 
@@ -17,6 +19,38 @@ namespace Bussines.implementations
             _audit = audit;
         }
 
+        public async Task<CreateAccountTypeDTO> create(CreateAccountTypeDTO request, string ip, string usuario)
+        {
+            var audit = await _audit.crearAuditoria("Crear tipos de cuenta", usuario, ip);
+            try
+            {
+                var result = await _data.Add(request.Clone<CreateAccountTypeDTO, TblAccountType>());
+                await _audit.exitoAuditoria((int)audit.IdLog, "Correcto");
+                return result.Clone<TblAccountType, CreateAccountTypeDTO>();
+            }
+            catch (Exception ex)
+            {
+                await _audit.falloAuditoria((int)audit.IdLog, "error:" + ex.Message);
+            }
+            return null;
+        }
+
+        public async Task<bool> delete(int id, string ip, string usuario)
+        {
+            var audit = await _audit.crearAuditoria("Eliminar tipos de cuenta", usuario, ip);
+            try
+            {
+                var deleted = await _data.delete(id);
+                await _audit.exitoAuditoria((int)audit.IdLog, "Correcto");
+                return deleted;
+            }
+            catch (Exception ex)
+            {
+                await _audit.falloAuditoria((int)audit.IdLog, "error:" + ex.Message);
+            }
+            return false;
+        }
+
         public async Task<ICollection<AccountTypeDTO>> get(string ip, string usuario)
         {
             var audit = await _audit.crearAuditoria("Consultar tipos de cuenta",usuario,ip);
@@ -29,6 +63,38 @@ namespace Bussines.implementations
             catch (Exception ex)
             {
                 await _audit.falloAuditoria((int)audit.IdLog, "error:"+ex.Message);
+            }
+            return null;
+        }
+
+        public async Task<CreateAccountTypeDTO> get(int id, string ip, string usuario)
+        {
+            var audit = await _audit.crearAuditoria("Consultar tipos de cuenta por id "+id, usuario, ip);
+            try
+            {
+                var result = await _data.get(id);
+                await _audit.exitoAuditoria((int)audit.IdLog, "Correcto");
+                return result.Clone<TblAccountType, CreateAccountTypeDTO>();
+            }
+            catch (Exception ex)
+            {
+                await _audit.falloAuditoria((int)audit.IdLog, "error:" + ex.Message);
+            }
+            return null;
+        }
+
+        public async Task<CreateAccountTypeDTO> update(CreateAccountTypeDTO request, string ip, string usuario)
+        {
+            var audit = await _audit.crearAuditoria("Actualizar tipos de cuenta", usuario, ip);
+            try
+            {
+                var result = await _data.Update(request.Clone<CreateAccountTypeDTO, TblAccountType>());
+                await _audit.exitoAuditoria((int)audit.IdLog, "Correcto");
+                return result.Clone<TblAccountType, CreateAccountTypeDTO>();
+            }
+            catch (Exception ex)
+            {
+                await _audit.falloAuditoria((int)audit.IdLog, "error:" + ex.Message);
             }
             return null;
         }
--- Bussines/Interfaces/IAccountTypeBussines.cs
+++ Bussines/Interfaces/IAccountTypeBussines.cs
@@ -1,9 +1,14 @@
-﻿using DTO.response;
+﻿using DTO.request;
+using DTO.response;
 
 namespace Bussines.Interfaces
 {
     public interface IAccountTypeBussines
     {
         public Task<ICollection<AccountTypeDTO>> get(string ip,string usuario);
+        public Task<CreateAccountTypeDTO> get(int id,string ip, string usuario);
+        public Task<CreateAccountTypeDTO> create(CreateAccountTypeDTO request, string ip, string usuario);
+        public Task<CreateAccountTypeDTO> update(CreateAccountTypeDTO request, string ip, string usuario);
+        public Task<bool> delete(int id, string ip, string usuario);
     }
 }
